821763049f9c290ca34095a904330b9c0d40c8d1
Ciro Cancello <cirocancello75@gmail.com>
2023-07-14 12:46:43 +0200

    pagina carrelllo.js

---
+++ FashionStore/carrello/carrello.js
@@ -0,0 +1,11 @@
+//getore degli eveneti
+
+function removeElement() {
+  let elemToRemove = document.getElementById("elem");
+  elemToRemove.parentNode.removeChild(elemToRemove);
+}
+
+function acquistaProdotti(){
+    let acquista = document.createEvent("acquistaButton");
+
+}
