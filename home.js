1c8b106e3af5feb6a237c9c6b62082b7202a40dd
GianmarcoGrimaldi <gianmarcogrimaldi.gg@gmail.com>
2023-07-14 13:16:45 +0200

    commit
    commit

---
+++ home.js
@@ -0,0 +1,11 @@
+document.getElementById("prodotti").addEventListener('click', function(){ 
+    window.location.href = 'prodotti.html' 
+}); 
+ 
+document.getElementById("carrello").addEventListener('click', function(){ 
+    window.location.href = 'carrello.html' 
+}); 
+ 
+document.getElementById("home").addEventListener('click', function(){ 
+    window.location.href = 'home.html' 
+});
