1e0c478fab77f392ae157b0f10114bebd76bc36b
debscalo <debora.caloiero@gmail.com>
2023-07-14 11:52:37 +0200

    update

---
+++ prodotti.js
@@ -0,0 +1,15 @@
+document.getElementById("prodotti").addEventListener('click', function(){
+    window.location.href = 'libreria.html'
+});
+
+document.getElementById("carrello").addEventListener('click', function(){
+    window.location.href = 'carrello.html'
+});
+
+document.getElementById("home").addEventListener('click', function(){
+    window.location.href = 'home.html'
+});
+
+let carrelloButton = document.getElementById("aggiungiAlCarrello")
+
+
